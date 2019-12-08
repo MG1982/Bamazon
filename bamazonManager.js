beae7ceb7be8fca7b6ec8e358e7a8ad4705fe3ec
Mani <manigonzalez82@gmail.com>
2019-12-08 22:50:47 +0800

    removed password

--- bamazonManager.js
+++ bamazonManager.js
@@ -17,7 +17,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "bootcamp",
+    password: "",
     database: "bamazon_DB"
 });
 
